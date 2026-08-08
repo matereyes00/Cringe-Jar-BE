12da3d8beca4dea91921437f5eb9d8f990f0bf09
Mate Reyes <martina.reyes.thesis@gmail.com>
2026-08-08 11:47:40 +1000

    mod: reworded routes for each request

--- src/infrastructure/http/room.controller.ts
+++ src/infrastructure/http/room.controller.ts
@@ -10,7 +10,7 @@ import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
-  @Post()
+  @Post('create-room')
   @ApiOperation({ summary: 'Create a new group room' })
   @ApiResponse({ status: 201, description: 'The room has been successfully created.' })
   @ApiResponse({ status: 403, description: 'Forbidden.' })
@@ -18,7 +18,7 @@ export class RoomsController {
     return this.roomsService.createRoom(dto);
   }
 
-  @Get(':id')
+  @Get('get-room/:id')
   @ApiOperation({ summary: 'View a specific group room using the existing group ID' })
   @ApiResponse({ status: 200, description: 'The room has been successfully retrieved.' })
   @ApiResponse({ status: 404, description: 'Room not found.' })
@@ -30,7 +30,19 @@ export class RoomsController {
     return this.roomsService.getRoom(roomId, passcode);
   }
 
-  @Post(':id/tally')
+  @Post(':id/add-member')
+  @ApiOperation({ summary: 'Add a member to a specific group room' })
+  @ApiResponse({ status: 200, description: 'The member has been successfully added.' })
+  @ApiResponse({ status: 404, description: 'Member not found.' })
+  async addMember(
+    @Param('id') id: string,
+    @Headers('x-passcode') passcode: string,
+    @Body() dto: CreateMemberInRoomDto,
+  ) {
+    return await this.roomsService.addMember(id, passcode, dto);
+  }
+
+  @Post(':id/tally-member')
   @ApiOperation({ summary: 'Add a tally to a member located in a specific group room' })
   @ApiResponse({ status: 200, description: 'The tally has been successfully added.' })
   @ApiResponse({ status: 404, description: 'Room not found.' })
@@ -41,17 +53,5 @@ export class RoomsController {
     @Body() dto: AddTallyDto,
   ) {
     return this.roomsService.addTally(roomId, passcode, dto);
-  }
-
-  @Post(':id/members')
-  @ApiOperation({ summary: 'Add a member to a specific group room' })
-  @ApiResponse({ status: 200, description: 'The member has been successfully added.' })
-  @ApiResponse({ status: 404, description: 'Member not found.' })
-  async addMember(
-    @Param('id') id: string,
-    @Headers('x-passcode') passcode: string,
-    @Body() dto: CreateMemberInRoomDto,
-  ) {
-    return await this.roomsService.addMember(id, passcode, dto);
   }
 }
